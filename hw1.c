f6369376e538ba73379a8c70276831a9ff5d3e36
Almir <leggetbruno@gmail.com>
2025-10-29 14:31:08 +0300

    start

---
+++ hw1.c
@@ -0,0 +1,17 @@
+/*
+ Write a 'shell-wrapper' program which allow users to:
+
+    - run programs via command line cyclically getting commands from STDIN and running 	    it somewhere, e.g. in child process.
+    - get exit codes of terminated programs
+    - use pipelining of program sequences, for ex.: «env | grep HOSTNAME | wc»
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+
+	return 0;
+
+}
